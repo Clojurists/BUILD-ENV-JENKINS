7be94f3c3417d8f720f46794c8824e4ff8cac6f1
Clojurists <92373035+Clojurists@users.noreply.github.com>
2020-09-27 09:01:49 +0000

    Atualizando lists properties

---
+++ share/genbuild.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -gt 0 ]; then
+    FILE="$1"
+    shift
+    if [ -f "$FILE" ]; then
+        INFO="$(head -n 1 "$FILE")"
+    fi
+else
+    echo "Usage : $0 <filename>"
+    exit 1
+fi
+
+if [ -e "$(which git)" ]; then
+    # clean 'dirty' status of touched files that haven't been modified
+    git diff >/dev/null 2>/dev/null 
+
+    # get a string like "v0.6.0-66-g59887e8-dirty"
+    DESC="$(git describe --dirty 2>/dev/null)"
