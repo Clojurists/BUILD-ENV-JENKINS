e5da21f1c094f05cad94b28ed762ef44babec73a
Clojurists <92373035+Clojurists@users.noreply.github.com>
2020-10-10 08:00:17 +0000

    style stuff

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -17,3 +17,19 @@ if [ -e "$(which git)" ]; then
 
     # get a string like "v0.6.0-66-g59887e8-dirty"
     DESC="$(git describe --dirty 2>/dev/null)"
+
+    # get a string like "2012-04-10 16:27:19 +0200"
+    TIME="$(git log -n 1 --format="%ci")"
+fi
+
+if [ -n "$DESC" ]; then
+    NEWINFO="#define BUILD_DESC \"$DESC\""
+else
+    NEWINFO="// No build information available"
+fi
+
+# only update build.h if necessary
+if [ "$INFO" != "$NEWINFO" ]; then
+    echo "$NEWINFO" >"$FILE"
+    echo "#define BUILD_DATE \"$TIME\"" >>"$FILE"
+fi
