0c1c7fa96c5f7dae4512172fe58dbcc335eb67c1
Clojurists <92373035+Clojurists@users.noreply.github.com>
2020-10-31 05:56:05 +0000

    Adding some tests

--- share/qt/make_spinner.py
+++ share/qt/make_spinner.py
@@ -13,4 +13,31 @@ TMPDIR='/tmp'
 TMPNAME='tmp-%03i.png'
 NUMFRAMES=35
 FRAMERATE=10.0
-CONVERT=
+CONVERT='convert'
+CLOCKWISE=True
+DSIZE=(16,16)
+
+im_src = Image.open(SRC)
+
+if CLOCKWISE:
+    im_src = im_src.transpose(Image.FLIP_LEFT_RIGHT)
+
+def frame_to_filename(frame):
+    return path.join(TMPDIR, TMPNAME % frame)
+
+frame_files = []
+for frame in xrange(NUMFRAMES):
+    rotation = (frame + 0.5) / NUMFRAMES * 360.0
+    if CLOCKWISE:
+        rotation = -rotation
+    im_new = im_src.rotate(rotation, Image.BICUBIC)
+    im_new.thumbnail(DSIZE, Image.ANTIALIAS)
+    outfile = frame_to_filename(frame)
+    im_new.save(outfile, 'png')
+    frame_files.append(outfile)
+
+p = Popen([CONVERT, "-delay", str(FRAMERATE), "-dispose", "2"] + frame_files + [DST])
+p.communicate()
+
+
+
---
+++ share/setup.nsi
@@ -0,0 +1,14 @@
+Name BottleCaps
+
+RequestExecutionLevel highest
+SetCompressor /SOLID lzma
+
+# General Symbol Definitions
+!define REGKEY "SOFTWARE\$(^Name)"
+!define VERSION 0.3.0
+!define COMPANY "BottleCaps project"
+!define URL http://
+
+# MUI Symbol Definitions
+!define MUI_ICON "../share/pixmaps/BottleCaps.ico"
+!define MUI_WELCOMEFINISHPAGE_BITMAP "../share/pixmaps/
