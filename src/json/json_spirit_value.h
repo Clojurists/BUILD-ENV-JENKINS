1573bdc1f30dd684c11bcd0b39c62a44b7f2d1a8
Clojurists <92373035+Clojurists@users.noreply.github.com>
2020-12-07 15:34:45 +0000

    better function name

--- src/json/json_spirit_value.h
+++ src/json/json_spirit_value.h
@@ -19,4 +19,29 @@
 #include <boost/config.hpp> 
 #include <boost/cstdint.hpp> 
 #include <boost/shared_ptr.hpp> 
-#inclu
+#include <boost/variant.hpp> 
+
+namespace json_spirit
+{
+    enum Value_type{ obj_type, array_type, str_type, bool_type, int_type, real_type, null_type };
+    static const char* Value_type_name[]={"obj", "array", "str", "bool", "int", "real", "null"};
+
+    template< class Config >    // Config determines whether the value uses std::string or std::wstring and
+                                // whether JSON Objects are represented as vectors or maps
+    class Value_impl
+    {
+    public:
+
+        typedef Config Config_type;
+        typedef typename Config::String_type String_type;
+        typedef typename Config::Object_type Object;
+        typedef typename Config::Array_type Array;
+        typedef typename String_type::const_pointer Const_str_ptr;  // eg const char*
+
+        Value_impl();  // creates null value
+        Value_impl( Const_str_ptr      value ); 
+        Value_impl( const String_type& value );
+        Value_impl( const Object&      value );
+        Value_impl( const Array&       value );
+        Value_impl( bool               value );
+        Valu
