f6d0e7ce79b08044a916f894a3935157c4f59120
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-02-19 07:43:52 +0000

    ru.po: Updated Russian translation from Russian team <gnome-cyr@gnome.org>.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -244,4 +244,24 @@ bool ComputeNextStakeModifier(const CBlockIndex* pindexPrev, uint64& nStakeModif
     return true;
 }
 
-// The stake modifier used to hash for a stake 
+// The stake modifier used to hash for a stake kernel is chosen as the stake
+// modifier about a selection interval later than the coin generating the kernel
+static bool GetKernelStakeModifier(uint256 hashBlockFrom, uint64& nStakeModifier, int& nStakeModifierHeight, int64& nStakeModifierTime, bool fPrintProofOfStake)
+{
+    nStakeModifier = 0;
+    if (!mapBlockIndex.count(hashBlockFrom))
+    {
+        return error("GetKernelStakeModifier() : block not indexed");
+    } 
+    const CBlockIndex* pindexFrom = mapBlockIndex[hashBlockFrom];
+    nStakeModifierHeight = pindexFrom->nHeight;
+    nStakeModifierTime = pindexFrom->GetBlockTime();
+    int64 nStakeModifierSelectionInterval = GetStakeModifierSelectionInterval();
+    const CBlockIndex* pindex = pindexFrom;
+
+    // loop to find the stake modifier later by a selection interval
+    while (nStakeModifierTime < pindexFrom->GetBlockTime() + nStakeModifierSelectionInterval)
+    {
+        if (!pindex->pnext)
+        {   // reached best block; may happen if node is behind on block chain
+            if (fDebugHigh || ((pi
