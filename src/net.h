a8f146581de5b081693ae88fbc4dced53bbeb167
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-03-18 13:36:09 +0000

    1.9.5

---
+++ src/net.h
@@ -0,0 +1,15 @@
+
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BITCOIN_NET_H
+#define BITCOIN_NET_H
+
+#include <deque>
+#include <boost/array.hpp>
+#include <boost/foreach.hpp>
+#include <openssl/rand.h>
+
+#ifndef WIN32
+#include <arpa/inet.h>
