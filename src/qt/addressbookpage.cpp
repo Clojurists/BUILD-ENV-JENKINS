d25019c4e922f5519cde3928d70a243451e6b41b
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-03-27 12:32:10 +0000

    Tidy up tests

--- src/qt/addressbookpage.cpp
+++ src/qt/addressbookpage.cpp
@@ -28,4 +28,40 @@ AddressBookPage::AddressBookPage(Mode mode, Tabs tab, QWidget *parent) :
     ui->setupUi(this);
 
 #ifdef Q_OS_MAC // Icons on push buttons are very uncommon on Mac
-    ui->newAddress->setIcon(QIcon(
+    ui->newAddress->setIcon(QIcon());
+    ui->copyAddress->setIcon(QIcon());
+    ui->deleteAddress->setIcon(QIcon());
+#endif
+
+#ifndef USE_QRCODE
+    ui->showQRCode->setVisible(false);
+#endif
+
+    switch (mode)
+    {
+      case ForSending:
+           connect(ui->tableView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(accept()));
+           ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+           ui->tableView->setFocus();
+           break;
+      case ForEditing:
+           ui->buttonBox->setVisible(false);
+           break;
+    }
+    switch (tab)
+    {
+      case SendingTab:
+           ui->labelExplanation->setVisible(false);
+           ui->deleteAddress->setVisible(true);
+           ui->signMessage->setVisible(false);
+           break;
+      case ReceivingTab:
+           ui->deleteAddress->setVisible(false);
+           ui->signMessage->setVisible(true);
+           break;
+    }
+
+    // Context menu actions
+    QAction *copyLabelAction = new QAction(tr("Copy &Label"), this);
+    QAction *copyAddressAction = new QAction(ui->copyAddress->text(), this);
+    QAction *editActi
