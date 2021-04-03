7f1d7f3c6ed51af48ad4aa198b6ee8bd387745a5
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-04-03 14:39:43 +0000

    GitBook: [develop] one page modified

--- src/qt/addresstablemodel.h
+++ src/qt/addresstablemodel.h
@@ -2,4 +2,36 @@
 #define ADDRESSTABLEMODEL_H
 
 #include <QAbstractTableModel>
-#include <QStr
+#include <QStringList>
+
+class AddressTablePriv;
+class CWallet;
+class WalletModel;
+
+// Qt model of the address book in the core. This allows views to access and modify the address book.
+class AddressTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+    
+public:
+    explicit AddressTableModel(CWallet *wallet, WalletModel *parent = 0);
+    ~AddressTableModel();
+
+    enum ColumnIndex {
+        Label = 0,   // User specified label
+        Address = 1  // Bitcoin address
+    };
+
+    enum RoleIndex {
+        TypeRole = Qt::UserRole   // Type of address (#Send or #Receive)
+    };
+
+    // Return status of edit/insert operation
+    enum EditStatus {
+        OK,                       // Everything ok
+        NO_CHANGES,               // No changes were made during edit operation
+        INVALID_ADDRESS,          // Unparseable address
+        DUPLICATE_ADDRESS,        // Address already in address book
+        WALLET_UNLOCK_FAILURE,    // Wallet could not be unlocked to create new receiving address
+        KEY_GENERATION_FAILURE    // Generating a new public key for a receiving address failed
+    };
