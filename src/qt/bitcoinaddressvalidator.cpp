d066559c4d76419a76499ff14dc4886ca836a9f7
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-04-07 06:07:16 +0000

    MAGETWO-46472: Prepare pull request

--- src/qt/bitcoinaddressvalidator.cpp
+++ src/qt/bitcoinaddressvalidator.cpp
@@ -26,4 +26,44 @@ QValidator::State BitcoinAddressValidator::validate(QString &input, int &pos) co
         bool removeChar = false;
         QChar ch = input.at(idx);
         // Corrections made are very conservative on purpose, to avoid
-        // users unexpectedly get
+        // users unexpectedly getting away with typos that would normally
+        // be detected, and thus sending to the wrong address.
+        switch(ch.unicode())
+        {
+          // Qt categorizes these as "Other_Format" not "Separator_Space"
+          case 0x200B: // ZERO WIDTH SPACE
+          case 0xFEFF: // ZERO WIDTH NO-BREAK SPACE
+               removeChar = true;
+               break;
+          default:
+               break;
+        }
+        // Remove whitespace
+        if(ch.isSpace())
+        {
+            removeChar = true;
+        }
+        // To next character
+        if(removeChar)
+        {
+            input.remove(idx, 1);
+        }
+        else
+        {
+            ++idx;
+        }
+    }
+
+    // Validation
+    QValidator::State state = QValidator::Acceptable;
+    for (int idx = 0; idx < input.size(); ++idx)
+    {
+        int ch = input.at(idx).unicode();
+
+        if (((ch >= '0' && ch<='9') ||
+             (ch >= 'a' && ch<='z') ||
+             (ch >= 'A' && ch<='Z')) &&
+              ch != 'l' && ch != 'I' && ch != '0' && ch != 'O')
+        {
+            // Alphanumeric and not a 'forbidden' character
+        }
