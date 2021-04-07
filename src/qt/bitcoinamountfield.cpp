ea99fe62256a024cad03a0ff3e37b68fd21f8bce
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-04-07 21:47:41 +0000

    Pontoon: Update Slovenian (sl) localization of Test Pilot Website

--- src/qt/bitcoinamountfield.cpp
+++ src/qt/bitcoinamountfield.cpp
@@ -12,4 +12,38 @@
 #include <QDoubleSpinBox>
 #include <QComboBox>
 #include <QApplication>
-#include
+#include <qmath.h>
+
+BitcoinAmountField::BitcoinAmountField(QWidget *parent):
+        QWidget(parent), amount(0), currentUnit(-1)
+{
+    amount = new QDoubleSpinBox(this);
+    amount->setLocale(QLocale::c());
+    amount->setDecimals(8);
+    amount->installEventFilter(this);
+    amount->setMaximumWidth(170);
+    amount->setSingleStep(0.001);
+
+    QHBoxLayout *layout = new QHBoxLayout(this);
+    layout->addWidget(amount);
+    unit = new QValueComboBox(this);
+    unit->setModel(new BitcoinUnits(this));
+    layout->addWidget(unit);
+    layout->addStretch(1);
+    layout->setContentsMargins(0,0,0,0);
+
+    setLayout(layout);
+
+    setFocusPolicy(Qt::TabFocus);
+    setFocusProxy(amount);
+
+    // If one if the widgets changes, the combined content changes as well
+    connect(amount, SIGNAL(valueChanged(QString)), this, SIGNAL(textChanged()));
+    connect(unit, SIGNAL(currentIndexChanged(int)), this, SLOT(unitChanged(int)));
+
+    // Set default based on configuration
+    unitChanged(unit->currentIndex());
+}
+
+
+void BitcoinAmountField::setText(co
