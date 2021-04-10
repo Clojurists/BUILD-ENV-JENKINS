ac614932ec251e188b872b51cc60f6bb1548ae8d
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-04-10 11:17:18 +0000

    some TODOs

---
+++ src/qt/bitcoingui.h
@@ -0,0 +1,95 @@
+
+#ifndef BITCOINGUI_H
+#define BITCOINGUI_H
+
+#include <QMainWindow>
+#include <QSystemTrayIcon>
+
+#include "util.h"
+
+class TransactionTableModel;
+class ClientModel;
+class WalletModel;
+class TransactionView;
+class OverviewPage;
+class AddressBookPage;
+class SendCoinsDialog;
+class SignVerifyMessageDialog;
+class Notificator;
+class RPCConsole;
+
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QLineEdit;
+class QTableView;
+class QAbstractItemModel;
+class QModelIndex;
+class QProgressBar;
+class QStackedWidget;
+class QUrl;
+QT_END_NAMESPACE
+
+// Bitcoin GUI main class. This class represents the main window of the Bitcoin UI. It communicates with both the client and
+// wallet models to give the user an up-to-date view of the current core state.
+class BitcoinGUI : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit BitcoinGUI(QWidget *parent = 0);
+    ~BitcoinGUI();
+
+    // Set the client model.
+    // The client model represents the part of the core that communicates with the P2P network, and is wallet-agnostic.
+    void setClientModel(ClientModel *clientModel);
+    // Set the wallet model.
+    // The wallet model represents a bitcoin wallet, and offers access to the list of transactions, address book and sending
+    // functionality.
+    void setWalletModel(WalletModel *walletModel);
+
+protected:
+    void changeEvent(QEvent *e);
+    void closeEvent(QCloseEvent *event);
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+
+private:
+    ClientModel *clientModel;
+    WalletModel *walletModel;
+
+    QStackedWidget *centralWidget;
+
+    OverviewPage *overviewPage;
+    QWidget *transactionsPage;
+    AddressBookPage *addressBookPage;
+    AddressBookPage *receiveCoinsPage;
+    SendCoinsDialog *sendCoinsPage;
+    SignVerifyMessageDialog *signVerifyMessageDialog;
+
+    QLabel *labelEncryptionIcon;
+    QLabel *labelMintingIcon;
+    QLabel *labelConnectionsIcon;
+    QLabel *labelBlocksIcon;
+    QLabel *progressBarLabel;
+    QProgressBar *progressBar;
+
+    QMenuBar *appMenuBar;
+    QAction *overviewAction;
+    QAction *historyAction;
+    QAction *quitAction;
+    QAction *sendCoinsAction;
+    QAction *addressBookAction;
+    QAction *signMessageAction;
+    QAction *verifyMessageAction;
+    QAction *aboutAction;
+    QAction *receiveCoinsAction;
+    QAction *optionsAction;
+    QAction *toggleHideAction;
+    QAction *exportAction;
+    QAction *encryptWalletAction;
+    QAction *backupWalletAction;
+    QAction *changePassphraseAction;
+    QAction *unlockWalletAction;
+    QAction *lockWalletAction;
+    QAction *aboutQtAction;
+    QAction *openRPCConsoleAction;
