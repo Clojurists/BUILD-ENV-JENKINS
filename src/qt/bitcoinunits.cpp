1642e8de915e64194b9a40e37d296e7b7510ee18
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-04-11 11:50:38 +0000

    Update generarReporte.js

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -43,4 +43,63 @@ QString BitcoinUnits::name(int unit)
            return QString("mJPC");
       case uBTC: 
            return QString::fromUtf8("μJPC");
- 
+      default: 
+           return QString("???");
+    }
+}
+
+
+QString BitcoinUnits::description(int unit)
+{
+    switch (unit)
+    {
+      case BTC: 
+           return QString("JackpotCoins");
+      case mBTC: 
+           return QString("Milli-JackpotCoins (1 / 1,000)");
+      case uBTC: 
+           return QString("Micro-JackpotCoins (1 / 1,000,000)");
+      default: 
+           return QString("???");
+    }
+}
+
+
+qint64 BitcoinUnits::factor(int unit)
+{
+    switch (unit)
+    {
+      case BTC:  
+           return 1000000;
+      case mBTC: 
+           return 1000;
+      case uBTC: 
+           return 1;
+      default:   
+           return 1000000;
+    }
+}
+
+
+int BitcoinUnits::amountDigits(int unit)
+{
+    switch (unit)
+    {
+      case BTC: 
+           return 8;  // 21,000,000 (# digits, without commas)
+      case mBTC: 
+           return 11; // 21,000,000,000
+      case uBTC: 
+           return 14; // 21,000,000,000,000
+      default: 
+           return 0;
+    }
+}
+
+
+int BitcoinUnits::decimals(int unit)
+{
+    switch(unit)
+    {
+      case BTC: 
+           return 6;
