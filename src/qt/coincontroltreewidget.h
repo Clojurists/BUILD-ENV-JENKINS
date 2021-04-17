e3bced22b215abfc711f530847b6fcf3f42cd2f9
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-04-17 15:27:13 +0000

    auto site generation

---
+++ src/qt/coincontroltreewidget.h
@@ -0,0 +1,15 @@
+#ifndef COINCONTROLTREEWIDGET_H
+#define COINCONTROLTREEWIDGET_H
+
+#include <QKeyEvent> 
+#include <QTreeWidget>
+
+class CoinControlTreeWidget : public QTreeWidget {
+
+Q_OBJECT
+
+public:
+   explicit CoinControlTreeWidget(QWidget *parent = 0);
+    
+protected:
+   vir
