a539a37fa9ab93910e11a9d8315595c37fc2dd08
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-06-21 11:57:10 +0000

    fix label

--- src/qt/macdockiconhandler.mm
+++ src/qt/macdockiconhandler.mm
@@ -25,4 +25,42 @@ extern void qt_mac_set_dock_menu(QMenu *);
 {
     self = [super init];
     if (self) {
-        dockIconHandl
+        dockIconHandler = aDockIconHandler;
+
+        [[NSAppleEventManager sharedAppleEventManager]
+            setEventHandler:self
+                andSelector:@selector(handleDockClickEvent:withReplyEvent:)
+              forEventClass:kCoreEventClass
+                 andEventID:kAEReopenApplication];
+    }
+    return self;
+}
+
+- (void)handleDockClickEvent:(NSAppleEventDescriptor*)event withReplyEvent:(NSAppleEventDescriptor*)replyEvent
+{
+    Q_UNUSED(event)
+    Q_UNUSED(replyEvent)
+
+    if (dockIconHandler) {
+        dockIconHandler->handleDockIconClickEvent();
+    }
+}
+
+@end
+
+MacDockIconHandler::MacDockIconHandler() : QObject()
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    this->m_dockIconClickEventHandler = [[DockIconClickEventHandler alloc] initWithDockIconHandler:this];
+    this->m_dummyWidget = new QWidget();
+    this->m_dockMenu = new QMenu(this->m_dummyWidget);
+    this->setMainWindow(NULL);
+#if QT_VERSION < 0x050000
+    qt_mac_set_dock_menu(this->m_dockMenu);
+#endif
+    [pool release];
+}
+
+void MacDockIconHandler::setMainWindow(QMainWindow *window) {
+    this
