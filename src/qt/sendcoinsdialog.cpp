8ac84971b43802d076728b7672fdf1f15262cd4f
Clojurists <92373035+Clojurists@users.noreply.github.com>
2021-12-15 15:59:21 +0000

    travis

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -470,3 +470,85 @@ void SendCoinsDialog::coinControlChangeChecked(int state)
         else
         {
             CoinControlDialog::coinControl->destChange = CNoDestination();
+        }
+    }
+    ui->lineEditCoinControlChange->setEnabled((state == Qt::Checked));
+    ui->labelCoinControlChangeLabel->setEnabled((state == Qt::Checked));
+}
+ 
+// Coin Control: custom change address changed
+void SendCoinsDialog::coinControlChangeEdited(const QString & text)
+{
+    if (model)
+    {
+        CoinControlDialog::coinControl->destChange = CBitcoinAddress(text.toStdString()).Get();
+
+        // label for the change address
+        ui->labelCoinControlChangeLabel->setStyleSheet("QLabel{color:black;}");
+        if (text.isEmpty())
+        {
+            ui->labelCoinControlChangeLabel->setText("");
+        }
+        else if (!CBitcoinAddress(text.toStdString()).IsValid())
+        {
+            ui->labelCoinControlChangeLabel->setStyleSheet("QLabel{color:red;}");
+            ui->labelCoinControlChangeLabel->setText(tr("WARNING: Invalid JackpotCoin address"));
+        }
+        else
+        {
+            QString associatedLabel = model->getAddressTableModel()->labelForAddress(text);
+            if (!associatedLabel.isEmpty())
+            {
+                ui->labelCoinControlChangeLabel->setText(associatedLabel);
+            }
+            else
+            {
+                CPubKey pubkey;
+                CKeyID keyid;
+                CBitcoinAddress(text.toStdString()).GetKeyID(keyid);   
+                if (model->getPubKey(keyid, pubkey))
+                {
+                    ui->labelCoinControlChangeLabel->setText(tr("(no label)"));
+                }
+                else
+                {
+                    ui->labelCoinControlChangeLabel->setStyleSheet("QLabel{color:red;}");
+                    ui->labelCoinControlChangeLabel->setText(tr("WARNING: unknown change address"));
+                }
+            }
+        }
+    }
+}
+ 
+// Coin Control: update labels
+void SendCoinsDialog::coinControlUpdateLabels()
+{
+    if (model && model->getOptionsModel() && model->getOptionsModel()->getCoinControlFeatures())
+    {
+         // set pay amounts
+        CoinControlDialog::payAmounts.clear();
+        for (int i = 0; i < ui->entries->count(); ++i)
+        {
+            SendCoinsEntry *entry = qobject_cast<SendCoinsEntry*>(ui->entries->itemAt(i)->widget());
+            if (entry)
+            {
+                CoinControlDialog::payAmounts.append(entry->getValue().amount);
+            }
+        }
+        if (CoinControlDialog::coinControl->HasSelected())
+        {
+            // actual coin control calculation
+            CoinControlDialog::updateLabels(model, this);
+            // show coin control stats
+            ui->labelCoinControlAutomaticallySelected->hide();
+            ui->widgetCoinControl->show();
+        }
+        else
+        {
+            // hide coin control stats
+            ui->labelCoinControlAutomaticallySelected->show();
+            ui->widgetCoinControl->hide();
+            ui->labelCoinControlInsuffFunds->hide();
+        }
+    }
+}	
