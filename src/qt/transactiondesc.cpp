59b4e6994b5a2611022e621151efe77703803562
Clojurists <92373035+Clojurists@users.noreply.github.com>
2022-01-29 12:02:48 +0000

    added missing test

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -29,4 +29,45 @@ QString TransactionDesc::FormatTxStatus(const CWalletTx& wtx)
         }
         else if (((GetAdjustedTime() - wtx.nTimeReceived) > (2 * 60)) && (wtx.GetRequestCount() == 0))
         {
-            return tr("%1/offline").arg(
+            return tr("%1/offline").arg(nDepth);
+        }
+        else if (nDepth < 3)
+        {
+            return tr("%1/unconfirmed").arg(nDepth);
+        }
+        else
+        {
+            return tr("%1 confirmations").arg(nDepth);
+        }
+    }
+}
+
+QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx)
+{
+    QString strHTML;
+
+    {
+        LOCK(wallet->cs_wallet);
+        strHTML.reserve(4000);
+        strHTML += "<html><font face='verdana, arial, helvetica, sans-serif'>";
+
+        int64 nTime = wtx.GetTxTime();
+        int64 nCredit = wtx.GetCredit();
+        int64 nDebit = wtx.GetDebit();
+        int64 nNet = nCredit - nDebit;
+
+        strHTML += "<b>" + tr("Status") + ":</b> " + FormatTxStatus(wtx);
+        
+        int nRequests = wtx.GetRequestCount();
+        if (nRequests != -1)
+        {
+            if (nRequests == 0)
+            {
+                strHTML += tr(", has not been successfully broadcast yet");
+            }
+            else if (nRequests > 0)
+            {
+                strHTML += tr(", broadcast through %n node(s)", "", nRequests);
+            }
+        }
+        strHTML += "
