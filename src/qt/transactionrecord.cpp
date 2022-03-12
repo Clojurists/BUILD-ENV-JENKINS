a5c842c106c093d9141a6a243426bec01441bb8b
Clojurists <92373035+Clojurists@users.noreply.github.com>
2022-03-12 16:58:44 +0000

    ACPI / PM: Fix resource_lock dead lock in acpi_power_on_device

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -20,4 +20,28 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet *
 {
     QList<TransactionRecord> parts;
     int64_t nTime = wtx.GetTxTime();
-    int64_
+    int64_t nCredit = wtx.GetCredit(true);
+    int64_t nDebit = wtx.GetDebit();
+    int64_t nNet = nCredit - nDebit;
+    uint256 hash = wtx.GetHash(), hashPrev = 0;
+    std::map<std::string, std::string> mapValue = wtx.mapValue;
+
+    if ((nNet > 0) || (wtx.IsCoinBase() || wtx.IsCoinStake()))
+    {
+        BOOST_FOREACH (const CTxOut& txout, wtx.vout)
+        {
+            if (wallet->IsMine(txout))
+            {
+                TransactionRecord sub(hash, nTime);
+                CTxDestination address;
+                sub.idx = parts.size(); // sequence number
+                sub.credit = txout.nValue;
+                if (ExtractDestination(txout.scriptPubKey, address) && IsMine(*wallet, address))
+                {
+                    // Received by Bitcoin Address
+                    sub.type = TransactionRecord::RecvWithAddress;
+                    sub.address = CBitcoinAddress(address).ToString();
+                }
+                else
+                {
+                    // Received by IP connection (deprecated features), or a multisignature or other non-simple transaction
