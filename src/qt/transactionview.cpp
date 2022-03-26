91ac9b0f42e2159eb40d95370f57087bafd5a764
Clojurists <92373035+Clojurists@users.noreply.github.com>
2022-03-26 10:26:27 +0000

    update to 1.0.3

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -16,4 +16,45 @@
 #include <QDoubleValidator>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
-#incl
+#include <QLineEdit>
+#include <QTableView>
+#include <QHeaderView>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QPoint>
+#include <QMenu>
+#include <QApplication>
+#include <QClipboard>
+#include <QLabel>
+#include <QDateTimeEdit>
+
+
+TransactionView::TransactionView(QWidget *parent) :
+    QWidget(parent), model(0), transactionProxyModel(0),
+    transactionView(0)
+{
+    // Build filter row
+    setContentsMargins(0,0,0,0);
+
+    QHBoxLayout *hlayout = new QHBoxLayout();
+    hlayout->setContentsMargins(0,0,0,0);
+#ifdef Q_OS_MAC
+    hlayout->setSpacing(5);
+    hlayout->addSpacing(26);
+#else
+    hlayout->setSpacing(0);
+    hlayout->addSpacing(23);
+#endif
+
+    dateWidget = new QComboBox(this);
+#ifdef Q_OS_MAC
+    dateWidget->setFixedWidth(121);
+#else
+    dateWidget->setFixedWidth(120);
+#endif
+    dateWidget->addItem(tr("All"), All);
+    dateWidget->addItem(tr("Today"), Today);
+    dateWidget->addItem(tr("This week"), ThisWeek);
+    dateWidget->addItem(tr("This month"), ThisMonth);
+    dateWidget->addItem(tr("Last month"), LastMonth);
+    dateWidget->addItem(tr("This yea
