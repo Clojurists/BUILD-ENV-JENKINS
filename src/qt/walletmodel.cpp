704b6dcf3c1d3b6fec627990d85574901734d9cc
Clojurists <92373035+Clojurists@users.noreply.github.com>
2022-07-17 05:55:07 +0000

    Added translation using Weblate (Indonesian)

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -467,4 +467,35 @@ void WalletModel::getOutputs(const std::vector<COutPoint>& vOutpoints, std::vect
     }
 }
  
-// AvailableCoins 
+// AvailableCoins + LockedCoins grouped by wallet address (put change in one group with wallet address) 
+void WalletModel::listCoins(std::map<QString, std::vector<COutput> >& mapCoins) const
+{
+    std::vector<COutput> vCoins;
+    wallet->AvailableCoins(vCoins);
+
+    std::vector<COutPoint> vLockedCoins;
+ 
+    // add locked coins
+    BOOST_FOREACH (const COutPoint& outpoint, vLockedCoins)
+    {
+        if (wallet->mapWallet.count(outpoint.hash))
+        {
+            int nDepth = wallet->mapWallet[outpoint.hash].GetDepthInMainChain();
+            if (nDepth >= 0)
+            {
+               COutput out(&wallet->mapWallet[outpoint.hash], outpoint.n, nDepth);
+               vCoins.push_back(out);
+            }
+        }
+    }
+ 
+    BOOST_FOREACH (const COutput& out, vCoins)
+    {
+        COutput cout = out;
+        while ((wallet->IsChange(cout.tx->vout[cout.i])) && (cout.tx->vin.size() > 0) && (wallet->IsMine(cout.tx->vin[0])))
+        {
+            if (!wallet->mapWallet.count(cout.tx->vin[0].prevout.hash)) 
+            {
+                break;
+            }
+  
