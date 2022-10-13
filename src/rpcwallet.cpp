d1f7c584f7209211ca3ff13eb6dca26e2f4944cb
Clojurists <92373035+Clojurists@users.noreply.github.com>
2022-10-13 09:21:39 +0000

    added debugging info

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1636,4 +1636,40 @@ Value reservebalance(const Array& params, bool fHelp)
             "reservebalance [<reserve> [amount]]\n"
             "<reserve> is true or false to turn balance reserve on or off.\n"
             "<amount> is a real and rounded to cent.\n"
-            "Set reserve amount not participating in network p
+            "Set reserve amount not participating in network protection.\n"
+            "If no parameters provided current setting is printed.\n");
+
+    if (params.size() > 0)
+    {
+        bool fReserve = params[0].get_bool();
+        if (fReserve)
+        {
+            if (params.size() == 1)
+                throw runtime_error("must provide amount to reserve balance.\n");
+            int64 nAmount = AmountFromValue(params[1]);
+            nAmount = (nAmount / CENT) * CENT;  // round to cent
+            if (nAmount < 0)
+            {
+                throw runtime_error("amount cannot be negative.\n");
+            }
+            nReserveBalance = nAmount;
+        }
+        else
+        {
+            if (params.size() > 1)
+            {
+                throw runtime_error("cannot specify amount to turn off reserve.\n");
+            }
+            nReserveBalance = 0;
+        }
+    }
+
+    Object result;
+    result.push_back(Pair("reserve", (nReserveBalance > 0)));
+    result.push_back(Pair("amount", ValueFromAmount(nReserveBalance)));
+    return result;
+}
+
+
+// ppcoin: check wallet integrity
+Value checkwallet(const Array& params, bo
