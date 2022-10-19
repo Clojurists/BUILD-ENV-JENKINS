2a4c02b473d569b8d796a725411eff911e6472c5
Clojurists <92373035+Clojurists@users.noreply.github.com>
2022-10-19 14:18:47 +0000

    add package info

---
+++ src/skein.c
@@ -0,0 +1,11 @@
+/* $Id: skein.c 254 2011-06-07 19:38:58Z tp $ */
+/*
+ * Skein implementation.
+ *
+ * ==========================(LICENSE BEGIN)============================
+ *
+ * Copyright (c) 2007-2010  Projet RNRT SAPHIR
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software wit
