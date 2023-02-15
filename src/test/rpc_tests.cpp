23978d32b5414b0a59fa36ee29e41e1f6b4351cc
Clojurists <92373035+Clojurists@users.noreply.github.com>
2023-02-15 10:04:24 +0000

    Minor whitespace fixes

--- src/test/rpc_tests.cpp
+++ src/test/rpc_tests.cpp
@@ -11,4 +11,31 @@ using namespace json_spirit;
 BOOST_AUTO_TEST_SUITE(rpc_tests)
 
 static Array
-create
+createArgs(int nRequired, const char* address1=NULL, const char* address2=NULL)
+{
+    Array result;
+    result.push_back(nRequired);
+    Array addresses;
+    if (address1) addresses.push_back(address1);
+    if (address2) addresses.push_back(address2);
+    result.push_back(addresses);
+    return result;
+}
+
+BOOST_AUTO_TEST_CASE(rpc_addmultisig)
+{
+    rpcfn_type addmultisig = tableRPC["addmultisigaddress"]->actor;
+
+    // old, 65-byte-long:
+    const char address1Hex[] = "0434e3e09f49ea168c5bbf53f877ff4206923858aab7c7e1df25bc263978107c95e35065a27ef6f1b27222db0ec97e0e895eaca603d3ee0d4c060ce3d8a00286c8";
+    // new, compressed:
+    const char address2Hex[] = "0388c2037017c62240b6b72ac1a2a5f94da790596ebd06177c8572752922165cb4";
+
+    Value v;
+    CBitcoinAddress address;
+    BOOST_CHECK_NO_THROW(v = addmultisig(createArgs(1, address1Hex), false));
+    address.SetString(v.get_str());
+    BOOST_CHECK(address.IsValid() && address.IsScript());
+
+    BOOST_CHECK_NO_THROW(v = addmultisig(createArgs(1, address1Hex, address2Hex), false));
+    address.SetString(v.get_str());
