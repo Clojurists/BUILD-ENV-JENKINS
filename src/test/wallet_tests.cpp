070df0122a49f094ce0f745a3ebf72a9dc095291
Clojurists <92373035+Clojurists@users.noreply.github.com>
2023-02-25 17:05:42 +0000

    wordpress

---
+++ src/test/wallet_tests.cpp
@@ -0,0 +1,9 @@
+#include <boost/test/unit_test.hpp>
+
+#include "main.h"
+#include "wallet.h"
+
+// how many times to run all the tests to have a chance to catch errors that only show up with particular random shuffles
+#define RUN_TESTS 100
+
+// some tes
